3dd93c8320e0d9c6b7242968c511884fd17f1f63
DeokJong <novelss444@jbnu.ac.kr>
2023-11-22 19:58:52 +0900

    edit php

--- writeform.php
+++ writeform.php
@@ -1,5 +1,16 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>JAVICE 자비스</title>
+    <meta charset="utf-8">
+    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body>
+    <?php
     // 폼 데이터 수신
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -11,14 +22,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // 예제: 저장된 데이터를 파일에 추가
     $data = "Name: $name\nEmail: $email\nMessage: $message\n\n";
     $file = 'form_data.txt';
-
-    // 파일에 데이터를 추가로 저장
     file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
 
     // 간단한 확인 메시지 출력
     echo "Data saved successfully.";
-} else {
-    // 폼이 제출되지 않은 경우 에러 메시지 출력
-    echo "Error: Form not submitted.";
-}
-?>
+    ?>
+
+</body>
+
+</html>
