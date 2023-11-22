0ec6a518781cd61b6ee3a740b468beb73d53183b
DeokJong <novelss444@jbnu.ac.kr>
2023-11-22 20:20:33 +0900

    test writeform

--- writeform.php
+++ writeform.php
@@ -11,20 +11,6 @@
 
 <body>
     <?php
-    // 폼 데이터 수신
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    // 여기에서 데이터를 저장하거나 다른 작업을 수행할 수 있습니다.
-    // 예를 들어, 데이터베이스에 저장하거나 이메일로 전송할 수 있습니다.
-
-    // 예제: 저장된 데이터를 파일에 추가
-    $data = "Name: $name\nEmail: $email\nMessage: $message\n\n";
-    $file = 'form_data.txt';
-    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-
-    // 간단한 확인 메시지 출력
     echo "Data saved successfully.";
     ?>
 
